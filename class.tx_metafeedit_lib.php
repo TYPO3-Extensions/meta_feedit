96529cef40351bdba79e88557d5f83364e8394ef
cpaige <cpaige@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-08-26 19:23:22 +0000

    adjust hook
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/meta_feedit/trunk@23683 735d13b6-9817-0410-8766-e36946ffe9aa

--- class.tx_metafeedit_lib.php
+++ class.tx_metafeedit_lib.php
@@ -2543,7 +2543,8 @@ class tx_metafeedit_lib {
 			mysql_free_result($res);
 
 			if ($conf['general.']['list.']['userFunc_alterSortTabs']) {
-				t3lib_div::callUserFunction($conf['general.']['list.']['userFunc_alterSortTabs'], $sortTab, $conf);
+			  $_params = array ('sortTab' => &$sortTab, 'sortAux' => &$sortAux, 'table' => $table, 'fN' => $fN  ); 
+        t3lib_div::callUserFunction($conf['general.']['list.']['userFunc_alterSortTabs'],  $_params , $conf);
 			}
 			
 			if ($statictable) {
