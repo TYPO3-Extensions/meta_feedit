847344f43c2e0bf1c4e943df83c98605db2c8487
cbalisky <cbalisky@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-07-06 12:11:38 +0000

    - added test case for filterFileNameCharacters
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/meta_feedit/trunk@89358 735d13b6-9817-0410-8766-e36946ffe9aa

--- Tests/MetaFeedit_Test.php
+++ Tests/MetaFeedit_Test.php
@@ -28,26 +28,37 @@
 require_once(t3lib_extMgm::extPath('meta_feedit').'class.tx_metafeedit_lib.php');
 
 class Tx_MetaFeedit_Tests_MetaFeedit_Test extends tx_phpunit_testcase {
-
-	protected $WSDLURI;
-	protected $SOAPServiceURI;
+	
+	private $metafeeditlib=null;
 
 	public function __construct($name = NULL, array $data = array(), $dataName = '') {
 		parent::__construct ($name, $data, $dataName);
+		$this->metafeeditlib=t3lib_div::makeInstance('tx_metafeedit_lib');
 	}
 	
 	/**
 	 * @test
 	 */
 	public function getMetaFeeditVar() {	
-		$metafeeditlib=t3lib_div::makeInstance('tx_metafeedit_lib');
 		$conf['pluginId']='id';
 		$conf['piVars']['test']['id']=4;
 		//$_POST['test[id]']=4;
-		$res=$metafeeditlib->getMetaFeeditVar($conf,'test');
+		$res=$this->metafeeditlib->getMetaFeeditVar($conf,'test');
 		self::assertTrue ($res==4, 'Couldnt get test value;'); 
 	}
+	/**
+	 * @test
+	 */
+	public function filterFileNameCharacters() {
 		
+		//$_POST['test[id]']=4;
+		$res=$this->metafeeditlib->filterFileNameCharacters('?*.|');
+		self::assertTrue ($res=='____', 'File name filter ko :'.$res);
+		$res=$this->metafeeditlib->filterFileNameCharacters('<">a');
+		self::assertTrue ($res=='___a', 'File name filter ko :'.$res);
+		$res=$this->metafeeditlib->filterFileNameCharacters("*'?b");
+		self::assertTrue ($res=='___b', 'File name filter ko :'.$res);
+	}
 }
 
 ?>
